8fd3d162bb86346f674e1b8881fb8675b8a07e7e
Seriusman Waruwu <seriusman0@gmail.com>
2021-07-06 14:08:32 +0700

    comment

---
+++ comment/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World, My Name is Serius")
+	//comment inline satu baris
+
+	/*multi line
+	comment*/
+
+	/*perulangan untuk mencetak angka 0 - 99
+	menggunakan looping*/
+	for i := 0; i < 100; i++ {
+		fmt.Println("perulangan ke - ", i)
+	}
+
+}
